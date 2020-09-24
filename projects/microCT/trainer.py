485d6a0376cfb8a1b26ad3a14d1b377ea92146e3
Aayush Kumar <aayushk61466@gmail.com>
2020-09-25 01:01:17 +0530

    Add files via upload

---
+++ projects/microCT/trainer.py
@@ -0,0 +1,360 @@
+import os, sys, glob 
+import time, itertools
+import GPUtil
+import pudb
+#pudb.set_trace()
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+from .solver import *
+from connectomics.model import *
+from connectomics.data.augmentation import build_train_augmentor, TestAugmentor
+from connectomics.data.dataset import build_dataloader, get_dataset
+from connectomics.data.utils import blend_gaussian, writeh5
+
+import torch.optim as optim
+import torchvision.transforms as transforms
+import os 
+from os import listdir
+import scipy.io
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import time
+import h5py
+
+import pandas as pd
+import numpy
+
+def seg_to_targets(label, topts):
+    out = [None]*len(topts)
+    for tid,topt in enumerate(topts):
+        if topt == '-1':
+            out[tid] = label[None,:].astype(np.float32)
+
+    return out
+
+base_dir = "CT_data/dsample"       # resampled volumes location (112,112,112)
+
+class Cthousefly(Dataset):  
+    def __init__(self, root_dir, transform=None): 
+        self.root_dir = root_dir
+        xl = pd.read_csv('/n/home03/aayushk614/aayush/exp2_new_mito/pytorch_connectomics/CT_data/train.csv')
+        self.trainfile = xl['Volumes']
+        self.groundtruths = xl['GT']
+        self.transform = transform
+        self.iter_num = 100000
+
+
+    def __len__(self):
+        return self.iter_num
+    
+    def __getitem__(self,idx):
+        
+        idx = idx % len(self.groundtruths)
+        volume_name=os.path.join(self.root_dir , self.trainfile[idx])
+        gt_name = os.path.join(self.root_dir, self.groundtruths[idx])
+
+        fid_v = h5py.File(volume_name, 'r')
+        dataset_v = list(fid_v)[0]
+
+        fid_gt = h5py.File(gt_name, 'r')
+        dataset_gt = list(fid_gt)[0]
+
+        volume = np.array(fid_v[dataset_v])
+
+        #volume = np.expand_dims(volume, axis=0)
+
+        ground_truth = np.array(fid_gt[dataset_gt])
+
+        ground_truth = seg_to_targets(ground_truth, ['-1']) 
+
+        
+        if self.transform:
+            volume = self.transform(volume)
+
+        return volume, ground_truth
+
+trainset = Cthousefly(root_dir="/n/home03/aayushk614/aayush/exp2_new_mito/pytorch_connectomics/CT_data/dsample")
+
+dataloader_simple = torch.utils.data.DataLoader(trainset, batch_size=1,shuffle=False, num_workers=4)
+
+
+class Trainer(object):
+    r"""Trainer
+
+    Args:
+        cfg: YACS configurations.
+        device (torch.device): by default all training and inference are conducted on GPUs.
+        mode (str): running mode of the trainer (``'train'`` or ``'test'``).
+        checkpoint (optional): the checkpoint file to be loaded (default: `None`)
+    """
+    def __init__(self, cfg, device, mode, checkpoint=None):
+        self.cfg = cfg
+        self.device = device
+        self.output_dir = cfg.DATASET.OUTPUT_PATH
+        self.mode = mode
+
+        self.model = build_model(self.cfg, self.device)
+        self.optimizer = build_optimizer(self.cfg, self.model)
+        self.lr_scheduler = build_lr_scheduler(self.cfg, self.optimizer)
+        self.start_iter = self.cfg.MODEL.PRE_MODEL_ITER
+        if checkpoint is not None:
+            self.update_checkpoint(checkpoint)
+
+        if self.mode == 'train':
+            self.augmentor = build_train_augmentor(self.cfg)
+        else:
+            self.augmentor = None
+        if cfg.DATASET.DO_CHUNK_TITLE == 0:
+            self.dataloader = build_dataloader(self.cfg, self.augmentor, self.mode)
+            self.dataloader = iter(dataloader_simple)
+        else:
+            self.dataset = None
+            self.dataloader = None
+        self.monitor = build_monitor(self.cfg)
+        self.criterion = nn.CrossEntropyLoss()
+
+        # add config details to tensorboard
+        self.monitor.load_config(self.cfg)
+        self.total_iter_nums = self.cfg.SOLVER.ITERATION_TOTAL - self.start_iter
+        self.inference_output_name = self.cfg.INFERENCE.OUTPUT_NAME
+
+
+    def train(self):
+        r"""Training function.
+        """
+        # setup
+        self.model.train()
+        self.monitor.reset()
+        self.optimizer.zero_grad()
+
+        for iteration in range(self.total_iter_nums):
+            iter_total = self.start_iter + iteration
+            start = time.perf_counter()
+            # load data
+            batch = next(self.dataloader)
+            volume, target = batch
+            time1 = time.perf_counter()
+
+            target_vis = target
+            
+            #print("volume :",volume.shape)
+            
+            volume = volume.to(self.device, dtype=torch.float)
+
+            volume = volume.unsqueeze(1)
+
+
+
+            target = target[0].to(self.device, dtype=torch.long)
+
+
+            target = np.squeeze(target, axis=1)
+
+            #print("Target :",target.shape)
+
+            #print("Target dtype :",target.dtype)
+
+            pred = self.model(volume)
+
+            pred_vis = pred.argmax(1)
+
+            pred_vis = pred_vis.unsqueeze(0).to(self.device, dtype=torch.float)
+
+            
+            #_, preds = torch.max(pred, 1)
+            loss = self.criterion(pred, target)
+
+
+            # compute gradient
+            loss.backward()
+            if (iteration+1) % self.cfg.SOLVER.ITERATION_STEP == 0:
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+
+            # logging and update record
+            do_vis = self.monitor.update(self.lr_scheduler, iter_total, loss, self.optimizer.param_groups[0]['lr']) 
+            
+
+            
+
+            if do_vis:
+
+
+                self.monitor.visualize(self.cfg, volume, target_vis, pred_vis, iter_total)
+                # Display GPU stats using the GPUtil package.
+                GPUtil.showUtilization(all=True)
+            
+
+            # Save model
+            if (iter_total+1) % self.cfg.SOLVER.ITERATION_SAVE == 0:
+                self.save_checkpoint(iter_total)
+
+            # update learning rate
+            self.lr_scheduler.step(loss) if self.cfg.SOLVER.LR_SCHEDULER_NAME == 'ReduceLROnPlateau' else self.lr_scheduler.step()
+
+            end = time.perf_counter()
+            print('[Iteration %05d] Data time: %.5f, Iter time:  %.5f' % (iter_total, time1 - start, end - start))
+
+            # Release some GPU memory and ensure same GPU usage in the consecutive iterations according to 
+            # https://discuss.pytorch.org/t/gpu-memory-consumption-increases-while-training/2770
+            del loss, pred
+
+    def test(self):
+        r"""Inference function.
+        """
+        if self.cfg.INFERENCE.DO_EVAL:
+            self.model.eval()
+        else:
+            self.model.train()
+        volume_id = 0
+
+        ww = blend_gaussian(self.cfg.MODEL.OUTPUT_SIZE)
+        NUM_OUT = self.cfg.MODEL.OUT_PLANES
+        pad_size = self.cfg.DATASET.PAD_SIZE
+        if len(self.cfg.DATASET.PAD_SIZE)==3:
+            pad_size = [self.cfg.DATASET.PAD_SIZE[0],self.cfg.DATASET.PAD_SIZE[0],
+                        self.cfg.DATASET.PAD_SIZE[1],self.cfg.DATASET.PAD_SIZE[1],
+                        self.cfg.DATASET.PAD_SIZE[2],self.cfg.DATASET.PAD_SIZE[2]]
+        
+        if ("super" in self.cfg.MODEL.ARCHITECTURE):
+            output_size = np.array(self.dataloader._dataset.input_size)*np.array(self.cfg.DATASET.SCALE_FACTOR).tolist()
+            result = [np.stack([np.zeros(x, dtype=np.float32) for _ in range(NUM_OUT)]) for x in output_size]
+            weight = [np.zeros(x, dtype=np.float32) for x in output_size]
+        else:
+            result = [np.stack([np.zeros(x, dtype=np.float32) for _ in range(NUM_OUT)]) for x in self.dataloader._dataset.input_size]
+            weight = [np.zeros(x, dtype=np.float32) for x in self.dataloader._dataset.input_size]
+
+        # build test-time augmentor and update output filename
+        output_filename = self.inference_output_name
+        if self.cfg.INFERENCE.AUG_NUM!=0:
+            test_augmentor = TestAugmentor(self.cfg.INFERENCE.AUG_MODE, 
+                                           self.cfg.INFERENCE.AUG_NUM)
+            output_filename = test_augmentor.update_name(output_filename)
+
+        start = time.time()
+        sz = tuple([NUM_OUT] + list(self.cfg.MODEL.OUTPUT_SIZE))
+        with torch.no_grad():
+            for _, (pos, volume) in enumerate(self.dataloader):
+                volume_id += self.cfg.INFERENCE.SAMPLES_PER_BATCH
+                print('volume_id:', volume_id)
+
+                # for gpu computing
+                volume = torch.from_numpy(volume).to(self.device)
+                if not self.cfg.INFERENCE.DO_3D:
+                    volume = volume.squeeze(1)
+
+                if self.cfg.INFERENCE.AUG_NUM!=0:
+                    output = test_augmentor(self.model, volume)
+                else:
+                    output = self.model(volume).cpu().detach().numpy()
+
+                if self.cfg.INFERENCE.MODEL_OUTPUT_ID[0] is not None: # select channel, self.cfg.INFERENCE.MODEL_OUTPUT_ID is a list [None]
+                    output = output[self.cfg.INFERENCE.MODEL_OUTPUT_ID[0]]
+                if not "super" in self.cfg.MODEL.ARCHITECTURE:
+                    for idx in range(output.shape[0]):
+                        st = pos[idx]
+                        result[st[0]][:, st[1]:st[1]+sz[1], st[2]:st[2]+sz[2], \
+                        st[3]:st[3]+sz[3]] += output[idx] * np.expand_dims(ww, axis=0)
+                        weight[st[0]][st[1]:st[1]+sz[1], st[2]:st[2]+sz[2], \
+                        st[3]:st[3]+sz[3]] += ww
+                else:
+                    for idx in range(output.shape[0]):
+                        st = pos[idx]
+                        st = (np.array(st)*np.array([1]+self.cfg.DATASET.SCALE_FACTOR)).tolist()
+                        result[st[0]][:, st[1]:st[1]+sz[1], st[2]:st[2]+sz[2], \
+                        st[3]:st[3]+sz[3]] += output[idx] * np.expand_dims(ww, axis=0)
+                        weight[st[0]][st[1]:st[1]+sz[1], st[2]:st[2]+sz[2], \
+                        st[3]:st[3]+sz[3]] += ww
+
+        end = time.time()
+        print("Prediction time:", (end-start))
+
+        for vol_id in range(len(result)):
+            if result[vol_id].ndim > weight[vol_id].ndim:
+                weight[vol_id] = np.expand_dims(weight[vol_id], axis=0)
+            # For segmentation masks, use uint16
+            result[vol_id] = (result[vol_id]/weight[vol_id]*255).astype(np.uint8)
+            sz = result[vol_id].shape
+            result[vol_id] = result[vol_id][:,
+                        pad_size[0]:sz[1]-pad_size[1],
+                        pad_size[2]:sz[2]-pad_size[3],
+                        pad_size[4]:sz[3]-pad_size[5]]
+
+        if self.output_dir is None:
+            return result
+        else:
+            print('save h5')
+            writeh5(os.path.join(self.output_dir, self.inference_output_name), result,['vol%d'%(x) for x in range(len(result))])
+
+    # -----------------------------------------------------------------------------
+    # Misc functions
+    # -----------------------------------------------------------------------------
+    def save_checkpoint(self, iteration):
+        state = {'iteration': iteration + 1,
+                 'state_dict': self.model.module.state_dict(), # Saving torch.nn.DataParallel Models
+                 'optimizer': self.optimizer.state_dict(),
+                 'lr_scheduler': self.lr_scheduler.state_dict()}
+                 
+        # Saves checkpoint to experiment directory
+        filename = 'checkpoint_%05d.pth.tar' % (iteration + 1)
+        filename = os.path.join(self.output_dir, filename)
+        torch.save(state, filename)
+
+    def update_checkpoint(self, checkpoint):
+        # load pre-trained model
+        print('Load pretrained checkpoint: ', checkpoint)
+        checkpoint = torch.load(checkpoint)
+        print('checkpoints: ', checkpoint.keys())
+        
+        # update model weights
+        if 'state_dict' in checkpoint.keys():
+            pretrained_dict = checkpoint['state_dict']
+            model_dict = self.model.module.state_dict() # nn.DataParallel
+            # 1. filter out unnecessary keys
+            pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+            # 2. overwrite entries in the existing state dict 
+            model_dict.update(pretrained_dict)    
+            # 3. load the new state dict
+            self.model.module.load_state_dict(model_dict) # nn.DataParallel   
+
+        if not self.cfg.SOLVER.ITERATION_RESTART:
+            # update optimizer
+            if 'optimizer' in checkpoint.keys():
+                self.optimizer.load_state_dict(checkpoint['optimizer'])
+
+            # update lr scheduler
+            if 'lr_scheduler' in checkpoint.keys():
+                self.lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
+
+            # load iteration
+            if 'iteration' in checkpoint.keys():
+                self.start_iter = checkpoint['iteration']
+
+    def run_chunk(self, mode):
+        self.dataset = get_dataset(self.cfg, self.augmentor, mode)
+        if mode == 'train':
+            num_chunk = self.total_iter_nums // self.cfg.DATASET.DATA_CHUNK_ITER
+            self.total_iter_nums = self.cfg.DATASET.DATA_CHUNK_ITER
+            for chunk in range(num_chunk):
+                self.dataset.updatechunk()
+                self.dataloader = build_dataloader(self.cfg, self.augmentor, mode, dataset=self.dataset.dataset)
+                self.dataloader = iter(self.dataloader)
+                print('start train', chunk)
+                self.train()
+                print('finished train', chunk)
+                self.start_iter += self.cfg.DATASET.DATA_CHUNK_ITER
+                del self.dataloader
+        else:
+            num_chunk = len(self.dataset.chunk_num_ind)
+            for chunk in range(num_chunk):
+                self.dataset.updatechunk(do_load=False)
+                self.inference_output_name = 'result-' + self.dataset.get_coord_name() + '.h5'
+                if not os.path.exists(os.path.join(self.output_dir, self.inference_output_name)):
+                    self.dataset.loadchunk()
+                    self.dataloader = build_dataloader(self.cfg, self.augmentor, mode, dataset=self.dataset.dataset)
+                    self.dataloader = iter(self.dataloader)
+                    self.test()
