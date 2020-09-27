e952f7ff9759fe6becd322f317e3b4178c658ba6
Aayush Kumar <aayushk61466@gmail.com>
2020-09-28 01:59:50 +0530

    Delete __init__.py

--- projects/microCT/solver/__init__.py
+++
@@ -1,3 +0,0 @@
-# Code adapted from Detectron2(https://github.com/facebookresearch/detectron2)
-from .build import build_lr_scheduler, build_optimizer
-from .lr_scheduler import WarmupCosineLR, WarmupMultiStepLR
